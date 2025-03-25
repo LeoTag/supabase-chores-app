a3e7a7e6bb46062a62711b7cfb9bebbad37deb6f
tagche <l.tagche@gmail.com>
2025-03-25 16:54:19 +0900

    Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import './assets/css/reset.css'
 import './assets/css/index.css'
 import './assets/css/App.css'
 import App from './App.tsx'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <SpeedInsights />
+        <App />
+    </StrictMode>,
 )
